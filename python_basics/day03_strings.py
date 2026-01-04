58365de1f477c609e994903b11fe4802e7c47813
Vishwajeet Patil <jeetai1523@gmail.com>
2026-01-05 02:52:00 +0530

    Day03-Strings and Input from User

---
+++ python_basics/day03_strings.py
@@ -0,0 +1,14 @@
+#Day3 - Strings and user Input
+name="vishwajeet"
+city="solapur"
+
+print("My name is",name)
+print("I live in",city)
+
+# Takin inputs from user
+
+user_name=input("Enter ue name:")
+user_age=input("Enter user age:")
+
+print("hello", user_name)
+print("Ur age is",user_age)
