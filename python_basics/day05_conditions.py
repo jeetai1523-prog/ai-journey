8c3f01703bc2e0c95cdc8269e837cdfb085c6add
Vishwajeet Patil <jeetai1523@gmail.com>
2026-01-05 14:04:27 +0530

    Day5 Conditions

---
+++ python_basics/day05_conditions.py
@@ -0,0 +1,23 @@
+#Conditions
+
+#IF-Else
+
+age=int(input("Enter your age"))
+
+if age>=18:
+    print("you are eligible to vote")
+else:
+    print("Not eligible to vote")
+
+#If-Else-If
+marks=int(input("Enter your marks"))
+
+if marks>67:
+    print("Distinction")
+elif marks>=60:
+    print("First Class ")
+elif marks>=40:
+    print("Passed")
+else:
+    print("Fail")
+
