fb769dd8027b8c160d1fc9a388f1cf3cc10b2522
Vishwajeet Patil <jeetai1523@gmail.com>
2026-01-05 11:17:04 +0530

    Day4 types and conversions

---
+++ python_basics/day04_type_conversions.py
@@ -0,0 +1,11 @@
+# Type Conversions
+
+age = input("Enter your Age- ")
+salary = input("Enter your Salary- ")
+
+age=int(age)
+salary=float(salary)
+
+print("My age is",age)
+print("Monthly salary",salary)
+
