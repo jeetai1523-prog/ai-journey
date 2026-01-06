ef2d9a6aad0d5f8f8e8ec6008e622286643ad32b
Vishwajeet Patil <jeetai1523@gmail.com>
2026-01-06 17:19:39 +0530

    Day6-Loops

---
+++ python_basics/day06_loop.py
@@ -0,0 +1,12 @@
+#Loop
+print("Using For loop ")
+
+for i in range(1,6):
+    print(i)
+
+#Multiplication Table
+
+number=int(input("Enter any number"))
+
+for i in range(1,11):
+    print(number,"*",i,"=", number*i)
