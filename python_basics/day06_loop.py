ea54e2f28261b6016700040d611059606d379569
Vishwajeet Patil <jeetai1523@gmail.com>
2026-01-11 00:54:10 +0530

    Loos- If and while loop done

--- python_basics/day06_loop.py
+++ python_basics/day06_loop.py
@@ -10,3 +10,16 @@ number=int(input("Enter any number"))
 
 for i in range(1,11):
     print(number,"*",i,"=", number*i)
+
+#While Loop
+
+print("Using While Loop")
+
+count=1
+while count<=6:
+    print(count)
+
+    count= count+1
+
+
+
