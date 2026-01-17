80823eab0a5a6832ffb41ea22fbd832eacd627e1
Vishwajeet Patil <jeetai1523@gmail.com>
2026-01-17 08:40:14 +0530

    Operators and Data types added

---
+++ python_basics/day02_variables_and_operators.py
@@ -0,0 +1,76 @@
+#day 2 Variables and numbers
+name="Monika"
+age=59
+height=5.2
+salary=580000
+
+print("My details are as follows")
+print("Name=",name)
+print("AGE=",age)
+print("HEIGHT=",height)
+print("SALARY=",salary)
+
+#Data Types
+print(type(age))
+print(type(height))
+print(type(name))
+print(type(salary))
+
+
+#Basic Arithmatic Operations
+a=10
+b=25
+
+print("Addition=",a+b)
+print("Subtraction=",a-b)
+print("Multiplication=",a*b)
+print("Division=",a/b)
+print("Rremainder",b%a)
+
+#Relational operators
+
+print(a==b)
+print(a!=b)
+print(a>=b)
+print(a<=b)
+
+#Assignment Operators
+num=15
+num=num+10
+print("num=",num)
+
+num+=4
+print("num=",num)
+num-=4
+print("num=",num)
+num*=4
+print("num=",num)
+num/=4
+print("num=",num)
+num%=4
+print("num=",num)
+num**=4
+print("num=",num)
+
+# a=20,55,65
+# print(a)
+
+#Logical operators
+
+#not
+print(False)
+print(not False)
+print(a>b)
+print(not a>b)
+
+#and
+value1=a<b
+value2=b<a
+print("AND operator=",value1 and value2)
+
+#or
+value1=a<b
+value2=b<a
+print("OR operator=",value1 or value2)
+
+
